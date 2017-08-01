146e13d59e9fec969068eaf5e6985de10e44f04a
Chris <chco@nodes.dk>
2017-08-01 16:21:01 +0200

    Fixing bug with existing fastlane dir

--- step.sh
+++ step.sh
@@ -3,6 +3,7 @@ set -ex
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ "${script_input}" == 'Fastlane copy' ]; then
+	if [ ! -d "$PWD/fastlane" ]
 	mkdir $PWD/fastlane
 	cp ${THIS_SCRIPT_DIR}/Fastfile $PWD/fastlane 
 elif [ "${script_input}" == 'Prep Slack message' ]; then
