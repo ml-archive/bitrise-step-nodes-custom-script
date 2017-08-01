d2806c81f20b8583ddfe680870172e2aba573537
Chris <chco@nodes.dk>
2017-08-01 16:21:38 +0200

    Bash syntax

--- step.sh
+++ step.sh
@@ -3,8 +3,9 @@ set -ex
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ "${script_input}" == 'Fastlane copy' ]; then
-	if [ ! -d "$PWD/fastlane" ]
-	mkdir $PWD/fastlane
+	if [ ! -d "$PWD/fastlane" ]; then
+		mkdir $PWD/fastlane
+	fi
 	cp ${THIS_SCRIPT_DIR}/Fastfile $PWD/fastlane 
 elif [ "${script_input}" == 'Prep Slack message' ]; then
 	ruby "${THIS_SCRIPT_DIR}/prepare_slack.rb"
