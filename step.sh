c7056b4e096cebe9a0a38212a2e6ffdbb9c34cce
Dominik Hádl <doha@nodesagency.com>
2018-09-17 09:45:47 +0200

    Improve fallbacks, remove redundant fastfile

--- step.sh
+++ step.sh
@@ -9,12 +9,19 @@ if [ "${script_input}" == 'Fastlane copy' ]; then
 		mkdir $PWD/fastlane
 	fi
 
-	# Try to load fastile for the correct CI version
+	# Check if we have ci-version set correctly
+	if [ -z "${CI_VERSION}" ]; then
+		# not set, fallback to v0.2
+		CI_VERSION='0.2'
+	fi
+
+		# Try to load fastile for the correct CI version
 	if [ -e "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Fastfile" ]; then 
 		cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Fastfile" $PWD/fastlane
 	else
-		# Ohterwise fallback to default fastfile
-		cp ${THIS_SCRIPT_DIR}/versions/Fastfile $PWD/fastlane
+		# Ohterwise fail
+		echo "No fastfile found in ci tools version folder ${CI_VERSION}."
+		exit 1
 	fi
 
 elif [ "${script_input}" == 'Prep Slack message' ]; then
