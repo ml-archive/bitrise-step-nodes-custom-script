f7ae4d8c2f2783d1d3458bdd3c8af1bec4c30484
Chris Combs <chco@nodes.dk>
2019-01-04 16:02:18 +0100

    Removing separate custom steps since they all use one fastfile

--- step.sh
+++ step.sh
@@ -2,42 +2,40 @@
 set -ex
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ "${script_input}" == 'Fastlane copy' ]; then
 
-	# if directory doesn't exist, create it
-	if [ ! -d "$PWD/fastlane" ]; then
-		mkdir $PWD/fastlane
-	fi
+# Parse project settings
+gem install hockeyver
+ruby "${THIS_SCRIPT_DIR}/parse_project_settings.rb"
 
-	# Check if we have ci-version set correctly
-	if [ -z "${CI_VERSION}" ]; then
-		# not set, fallback to v0.2
-		CI_VERSION='0.2'
-	fi
+# Copy fastlane files
 
-		# Try to load fastile for the correct CI version
-	if [ -e "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Fastfile" ]; then
-		cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Fastfile" $PWD/fastlane
-	else
-		# Otherwise fail
-		echo "No fastfile found in ci tools version folder ${CI_VERSION}."
-		exit 1
-	fi
+# if directory doesn't exist, create it
+if [ ! -d "$PWD/fastlane" ]; then
+	mkdir $PWD/fastlane
+fi
 
-	# Check for plugin capabilities
-	if [[ -e "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Pluginfile" &&
-					"${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Gemfile" &&
-	 				"${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Gemfile.lock" ]]; then
-		cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Pluginfile" $PWD/fastlane
-		cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Gemfile" $PWD
-		cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Gemfile.lock" $PWD
+# Check if we have ci-version set correctly
+if [ -z "${CI_VERSION}" ]; then
+	# not set, fallback to v0.2
+	CI_VERSION='0.2'
+fi
 
-		bundle exec fastlane install_plugins
-	fi
-
-elif [ "${script_input}" == 'Prep Slack message' ]; then
-	ruby "${THIS_SCRIPT_DIR}/prepare_slack.rb"
+	# Try to load fastile for the correct CI version
+if [ -e "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Fastfile" ]; then
+	cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Fastfile" $PWD/fastlane
 else
-	gem install hockeyver
-	ruby "${THIS_SCRIPT_DIR}/parse_project_settings.rb"
+	# Otherwise fail
+	echo "No fastfile found in ci tools version folder ${CI_VERSION}."
+	exit 1
+fi
+
+# Check for plugin capabilities
+if [[ -e "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Pluginfile" &&
+				"${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Gemfile" &&
+				"${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Gemfile.lock" ]]; then
+	cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Pluginfile" $PWD/fastlane
+	cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Gemfile" $PWD
+	cp "${THIS_SCRIPT_DIR}/versions/${CI_VERSION}/Gemfile.lock" $PWD
+
+	bundle exec fastlane install_plugins
 fi
